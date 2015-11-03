139f6896ed4396b2e48d643b61b99cd23546c5fc
shomit78 <shomit78@hotmail.com>
2015-11-03 16:56:33 +0000

    Initial commit

---
+++ chome/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(chome.Startup))]
+namespace chome
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
